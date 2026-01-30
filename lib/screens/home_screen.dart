1b462183624c6d58f2cd45b4370bb3031481ecd9
stephen-gabriel <stephengabriel123401@gmail.com>
2026-01-30 01:34:56 +0100

    Implemented Styling to Header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,17 +9,43 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(children: [
-                  
-                ],
-              ),
-              Row(children: [
-                  
-                ],
-              ),
-            ],
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: AppStyles.headLineStyle3,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headLineStyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Search icon"),
+                    Text("Empty space"),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
