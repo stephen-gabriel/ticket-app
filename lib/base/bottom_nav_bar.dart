3b29186d744dc401214300b5ef000f75610c15a6
stephen-gabriel <stephengabriel123401@gmail.com>
2026-02-03 17:11:50 +0100

    Created reusable widgets

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -29,10 +29,6 @@ class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Tickets"),
-        centerTitle: true,
-      ),
       body: appScreens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -4,11 +4,22 @@ Color primary = const Color(0xFF687daf);
 
 class AppStyles {
   static Color primaryColor = primary;
+  static Color bgColor = const Color(0xFFEEEDF2);
   static Color textColor = const Color(0xFF3b3b3b);
 
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: textColor,
+  );
   static TextStyle headLineStyle1 = TextStyle(
     fontSize: 26,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+  static TextStyle headLineStyle2 = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
     color: textColor,
   );
   static TextStyle headLineStyle3 = const TextStyle(
